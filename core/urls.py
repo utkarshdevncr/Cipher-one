9fedb21fe0563d21853d2212abbc9dd21125cde3
devutk-cont <63770449+devutk-cont@users.noreply.github.com>
2020-09-15 13:37:25 +0530

    commit

---
+++ core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+
+from .views import *
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('add-listing', CreateListing.as_view(), name='add_listing'),
+    path('listings/<slug:slug>/', ListingView.as_view(), name='listing'),
+    path('addInfo',addInfo,name='addInfo'),
+    path('lists',lists,name='lists'),
+    path('diss',dissatisfy, name='diss'),
+]
